91b2b859fed56edbf28311ded466c9867c872060
Mattias Holmqvist <mattias@serialized.io>
2020-08-20 12:11:26 +0200

    Extracted common urls to methods

--- lib/FeedsClient.ts
+++ lib/FeedsClient.ts
@@ -34,10 +34,14 @@ export interface LoadFeedsOverviewResponse {
   feeds: FeedOverview[];
 }
 
-export interface LoadFeedRequest {
+export interface FeedRequest {
   feedName: string;
+}
+
+export interface LoadFeedRequest extends FeedRequest {
   paginationOptions?: PaginationOptions;
 }
+
 
 export interface LoadAllFeedRequest {
   feedName: string;
@@ -51,28 +55,41 @@ export class FeedsClient extends BaseClient {
   }
 
   public async loadOverview(): Promise<LoadFeedsOverviewResponse> {
-    return (await this.axiosClient.get(`/feeds`)).data;
+    return (await this.axiosClient.get(FeedsClient.feedsUrl())).data;
   }
 
   public async loadFeed(request: LoadFeedRequest): Promise<LoadFeedResponse> {
     const config = this.axiosConfig();
     config.params = request.paginationOptions;
-    return (await this.axiosClient.get(`/feeds/${request.feedName}`, config)).data;
+    return (await this.axiosClient.get(FeedsClient.feedUrl(request.feedName), config)).data;
+  }
+
+  public async getCurrentSequenceNumber(feed: FeedRequest): Promise<number> {
+    const headers = (await this.axiosClient.head(FeedsClient.feedUrl(feed.feedName))).headers;
+    return headers['Serialized-SequenceNumber-Current']
   }
 
   public async loadAllFeed(request: LoadAllFeedRequest): Promise<LoadFeedResponse> {
     const config = this.axiosConfig();
     config.params = request.paginationOptions;
-    return (await this.axiosClient.get(`/feeds/_all`, config)).data;
-  }
-
-  public async getCurrentSequenceNumber(feedName: string): Promise<number> {
-    const headers = (await this.axiosClient.head(`/feeds/${feedName}`)).headers;
-    return headers['Serialized-SequenceNumber-Current']
+    return (await this.axiosClient.get(FeedsClient.allFeedUrl(), config)).data;
   }
 
   public async getGlobalSequenceNumber(): Promise<number> {
-    const headers = (await this.axiosClient.head(`/feeds/_all`)).headers;
+    const headers = (await this.axiosClient.head(FeedsClient.allFeedUrl())).headers;
     return headers['Serialized-SequenceNumber-Current']
   }
+
+  public static feedsUrl() {
+    return `/feeds`;
+  }
+
+  public static allFeedUrl() {
+    return `/feeds/_all`;
+  }
+
+  public static feedUrl(feedName: string) {
+    return `/feeds/${feedName}`;
+  }
+
 }
