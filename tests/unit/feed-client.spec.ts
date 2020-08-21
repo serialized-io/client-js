32488f8c1e14ce5b2d08c83348c6824fbfd3234c
Mattias Holmqvist <mattias@serialized.io>
2020-08-21 09:45:05 +0200

    Added tests for feed client

---
+++ tests/unit/feed-client.spec.ts
@@ -0,0 +1,67 @@
+import {
+  FeedPaginationOptions,
+  FeedsClient,
+  LoadFeedResponse,
+  LoadFeedsOverviewResponse,
+  Serialized,
+  SerializedInstance
+} from "../../lib";
+
+const uuidv4 = require("uuid").v4;
+const {randomKeyConfig, mockClient, mockGetOk} = require("./client-helpers");
+
+describe('Feed client', () => {
+
+  it('Can list feeds', async () => {
+    const serializedInstance: SerializedInstance = Serialized.create(randomKeyConfig())
+
+    const expectedResponse: LoadFeedsOverviewResponse = {
+      feeds: [{
+        aggregateCount: 1,
+        aggregateType: 'user-registration',
+        batchCount: 1,
+        eventCount: 1
+      }
+      ]
+    }
+
+    mockClient(
+        serializedInstance.axiosClient,
+        [mockGetOk(RegExp(`^${FeedsClient.feedsUrl()}$`), expectedResponse)]);
+
+    const response = await serializedInstance.feeds.loadOverview();
+    expect(response).toStrictEqual(expectedResponse)
+  })
+
+  it('Can load feed with pagination', async () => {
+
+    const serializedInstance: SerializedInstance = Serialized.create(randomKeyConfig())
+    const aggregateId = uuidv4();
+    const expectedResponse: LoadFeedResponse = {
+      currentSequenceNumber: 10,
+      entries: [{
+        aggregateId: aggregateId,
+        events: [],
+        feedName: '',
+        sequenceNumber: 1,
+        timestamp: 0
+      }],
+      hasMore: false
+    }
+    const requestOptions: FeedPaginationOptions = {
+      since: 0,
+      limit: 10
+    }
+
+    mockClient(
+        serializedInstance.axiosClient,
+        [mockGetOk(RegExp(`^${FeedsClient.feedUrl('user-registration')}$`), expectedResponse)]);
+
+    const response = await serializedInstance.feeds.loadFeed({
+      feedName: 'user-registration',
+      options: requestOptions
+    });
+    expect(response).toStrictEqual(expectedResponse)
+  });
+
+})
