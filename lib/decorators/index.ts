fefd8bc0abb20eccf29314bec398da828b2a8bf1
Mattias Holmqvist <mattias@serialized.io>
2021-04-08 09:42:51 +0200

    Removed function comments

--- lib/decorators/index.ts
+++ lib/decorators/index.ts
@@ -1,10 +1,3 @@
-/**
- * 2021-04-07 by MrSpoocy: Fix issue if you use ES6 or higher (https://github.com/vuex-orm/vuex-orm/commit/25b458bd4de72f64ad4e8b3a3983ace062be8cfd)
- *
- * @param {string} aggregateType
- * @param eventHandlersType
- * @constructor
- */
 export function Aggregate(aggregateType: string, eventHandlersType) {
   return function handlesEventsFor<T extends { new(...args: any[]): {} }>(constructor: T) {
     let proxy: T
