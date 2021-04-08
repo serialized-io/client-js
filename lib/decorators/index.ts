84fa8bbc07898a5dfbb51241d0dede0886206ca2
Manuel <github@mrspoocy.com>
2021-04-08 08:59:42 +0200

    Support ES6 and higher
    Based on this problem https://babeljs.io/docs/en/babel-plugin-transform-classes here is the fix that we already talk in a chat.

--- lib/decorators/index.ts
+++ lib/decorators/index.ts
@@ -1,12 +1,30 @@
+/**
+ * 2021-04-07 by MrSpoocy: Fix issue if you use ES6 or higher (https://github.com/vuex-orm/vuex-orm/commit/25b458bd4de72f64ad4e8b3a3983ace062be8cfd)
+ *
+ * @param {string} aggregateType
+ * @param eventHandlersType
+ * @constructor
+ */
 export function Aggregate(aggregateType: string, eventHandlersType) {
-  return function handlesEventsFor<T extends { new(...args: any[]): {} }>(
-      constructor: T
-  ) {
-    return class extends constructor {
-      aggregateType = aggregateType;
-      initialState = eventHandlersType.prototype.initialState;
-      eventHandlers = eventHandlersType.eventHandlers;
-    };
+  return function handlesEventsFor<T extends { new(...args: any[]): {} }>(constructor: T) {
+    let proxy: T
+    try {
+      proxy = new Function('constructor', 'aggregateType', 'eventHandlersType', `
+        'use strict';
+        return class extends constructor {
+            aggregateType = aggregateType;
+            initialState = eventHandlersType.prototype.initialState;
+            eventHandlers = eventHandlersType.eventHandlers;
+        };
+    `)(constructor, aggregateType, eventHandlersType);
+    } catch {
+      proxy = class extends constructor {
+        aggregateType = aggregateType;
+        initialState = eventHandlersType.prototype.initialState;
+        eventHandlers = eventHandlersType.eventHandlers;
+      }
+    }
+    return proxy;
   }
 }
 
